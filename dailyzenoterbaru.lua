a650082f00f0f25dddff778ea7a080ba9e90cea4
pastikadipawork01-bit <pastikadipawork01@gmail.com>
2026-04-12 01:48:27 +0700

    Update dailyzenoterbaru.lua

--- dailyzenoterbaru.lua
+++ dailyzenoterbaru.lua
@@ -308,13 +308,8 @@ end)
         end
     end
 	local function claimAllMail()
-    pcall(function()
-        game:GetService("ReplicatedStorage")
-            :WaitForChild("\228\186\139\228\187\182")
-            :WaitForChild("\229\133\172\231\148\168")
-            :WaitForChild("\233\130\174\228\187\182")
-            :WaitForChild("\233\162\134\229\143\150\229\133\168\233\131\168\233\130\174\228\187\182")
-            :FireServer()
+    game:GetService("ReplicatedStorage"):WaitForChild("\228\186\139\228\187\182"):WaitForChild("\229\133\172\231\148\168"):WaitForChild("\233\130\174\228\187\182"):WaitForChild("\233\162\134\229\143\150\229\133\168\233\131\168\233\130\174\228\187\182"):FireServer()
+
     end)
 end
 
